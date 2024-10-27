7cd555ea55b22b192d15fe9f12c57f3e7e733d7e
Abdul1091 <111249085+Abdul1091@users.noreply.github.com>
2024-10-27 02:11:23 +0100

    Add a paragrph with red text

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 // your JavaScript file
+//const container = document.querySelector("#container");
+
+//const content = document.createElement("div");
+//content.classList.add("content");
+//content.textContent = "This is the glorious text-content!";
+
+//container.appendChild(content);
+
+// Reference to the container div
 const container = document.querySelector("#container");
 
+// First div with class 'content'
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-
 container.appendChild(content);
+
+// Add a <p> with red text
+const redText = document.createElement("p");
+redText.style.color = "red";
+redText.textContent = "Hey I'm red!";
+container.appendChild(redText);
